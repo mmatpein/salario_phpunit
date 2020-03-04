e971b988111d6d45f0b43a781a433d7ad48d70a0
Mauricio Matamala <mauricio.politecnico@gmail.com>
2020-03-04 19:55:22 +0100

    Cambio en index.php

--- aprendophpunit/index.php
+++ aprendophpunit/index.php
@@ -9,7 +9,7 @@
 require "../vendor/autoload.php";
 use aprendophpunit\modelo\Salario;
 $salario = new Salario();
-echo "El aumento de salario es ".$salario->incrementoSalario(2000);
+echo "El nuevo aumento de salario es ".$salario->incrementoSalario(2000);
 ?>
 </body>
 </html>
